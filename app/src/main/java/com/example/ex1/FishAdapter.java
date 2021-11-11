b9db76e1e12568f137231eaef46cb3777f6ad3a4
choy30 <alcls0026@gmail.com>
2021-11-12 04:22:23 +1300

    diagnose btnupdate update data

--- app/src/main/java/com/example/ex1/FishAdapter.java
+++ app/src/main/java/com/example/ex1/FishAdapter.java
@@ -1,19 +1,27 @@
 package com.example.ex1;
 
+import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
 import com.orhanobut.dialogplus.DialogPlus;
 import com.orhanobut.dialogplus.ViewHolder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FishAdapter extends FirebaseRecyclerAdapter <Fish, FishAdapter.fishViewHolder> {
 
@@ -28,7 +36,7 @@ public class FishAdapter extends FirebaseRecyclerAdapter <Fish, FishAdapter.fish
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull fishViewHolder holder, int position, @NonNull Fish model) {
+    protected void onBindViewHolder(@NonNull fishViewHolder holder, @SuppressLint("RecyclerView") int position, @NonNull Fish model) {
         holder.name.setText(model.getName());
         holder.breed.setText(model.getBreed());
         holder.age.setText(model.getAge());
@@ -58,7 +66,33 @@ public class FishAdapter extends FirebaseRecyclerAdapter <Fish, FishAdapter.fish
 
                 dialogPlus.show();
 
+                btnupdate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Map<String, Object> map = new HashMap<>();
+                        map.put("name", name.getText().toString());
+                        map.put("breed", breed.getText().toString());
+                        map.put("age", age.getText().toString());
+                        map.put("weight", weight.getText().toString());
 
+                        FirebaseDatabase.getInstance().getReference().child("fishes")
+                                .child(getRef(position).getKey()).updateChildren(map)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(holder.name.getContext(), "Updated", Toast.LENGTH_SHORT).show();
+                                        dialogPlus.dismiss();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(holder.name.getContext(), "Update Failed", Toast.LENGTH_SHORT).show();
+                                        dialogPlus.dismiss();
+                                    }
+                                });
+                    }
+                });
             }
         });
 
