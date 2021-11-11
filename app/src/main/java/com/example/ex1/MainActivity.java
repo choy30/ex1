23d264174bbf4cbc4a32f05ab97573227ae7f393
choy30 <alcls0026@gmail.com>
2021-11-12 05:12:16 +1300

    Add feature added

--- app/src/main/java/com/example/ex1/MainActivity.java
+++ app/src/main/java/com/example/ex1/MainActivity.java
@@ -5,11 +5,20 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText name, breed, age, weight;
+    Button btnadd;
 
     RecyclerView recyclerView;
     FishAdapter fishAdapter;
@@ -18,6 +27,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = (EditText)findViewById(R.id.txt_name);
+        breed = (EditText)findViewById(R.id.txt_breed);
+        age = (EditText)findViewById(R.id.txt_age);
+        weight = (EditText)findViewById(R.id.txt_weight);
+
+        btnadd = (Button)findViewById(R.id.btn_add);
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataAdd();
+            }
+        });
 
         recyclerView = (RecyclerView) findViewById(R.id.lst_fish);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -30,6 +52,18 @@ public class MainActivity extends AppCompatActivity {
 
         fishAdapter = new FishAdapter(options);
         recyclerView.setAdapter(fishAdapter);
+
+    }
+
+    private void dataAdd() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", name.getText().toString());
+        map.put("breed", breed.getText().toString());
+        map.put("age", age.getText().toString());
+        map.put("weight", weight.getText().toString());
+
+        FirebaseDatabase.getInstance().getReference().child("fishes").push()
+                .setValue(map);
     }
 
     @Override
